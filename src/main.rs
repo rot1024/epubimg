e13eeeb6846b59161ee4260a0f191266f8700d20
rot1024 <aayhrot@gmail.com>
2021-10-11 23:32:29 +0900

    support dir

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use regex::Regex;
 use std::{
     borrow::Cow,
     error::Error,
-    fs::{create_dir, File},
+    fs::{create_dir, metadata, read_dir, File},
     io::copy,
-    path::Path,
+    path::{Path, PathBuf},
     process::exit,
 };
 use zip::ZipArchive;
@@ -30,11 +30,27 @@ fn main() {
 fn main2() -> Result<(), String> {
     let args = App::parse();
 
-    if args.files.is_empty() {
+    let files = args
+        .files
+        .into_iter()
+        .map(|p| {
+            metadata(&p).and_then(|m| {
+                if m.is_dir() {
+                    read_dir(&p).map(|d| d.filter_map(|e| e.ok()).map(|e| e.path()).collect())
+                } else {
+                    Ok(vec![PathBuf::from(p)])
+                }
+            })
+        })
+        .filter_map(|r| r.ok())
+        .flatten()
+        .collect::<Vec<_>>();
+
+    if files.is_empty() {
         return Err("At least one file must be specified. ".to_string());
     }
 
-    for p in args.files {
+    for p in files {
         process(&p, args.fullname).map_err(|e| e.to_string())?;
     }
 
@@ -43,12 +59,12 @@ fn main2() -> Result<(), String> {
     Ok(())
 }
 
-fn process(p: &str, fullname: bool) -> Result<(), Box<dyn Error>> {
+fn process<P: AsRef<Path>>(path: P, fullname: bool) -> Result<(), Box<dyn Error>> {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"【.+?】|\(.+? Edition\)").unwrap();
     }
+    let path = path.as_ref();
 
-    let path = Path::new(&p);
     let destdirname = match path.file_stem().and_then(|n| n.to_str()) {
         Some(f) => f,
         None => return Ok(()),
